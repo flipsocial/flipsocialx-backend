f18298b51aebfa68e7819ca09a331fd63c8f4baa
Juan Carlos <flipsocialx@gmail.com>
2025-07-11 03:03:15 -0500

    Agrega ruta POST /upload

--- server.js
+++ server.js
@@ -9,11 +9,6 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-app.post('/upload', (req, res) => {
-  console.log("Solicitud recibida:", req.body);
-  res.status(200).json({ message: 'Recibido en el backend 🚀' });
-});
-
 // Supabase config
 const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
 
@@ -34,4 +29,11 @@ app.get('/videos', async (req, res) => {
 
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en puerto ${PORT}`);
+});
+
+app.use(express.json());
+
+app.post('/upload', (req, res) => {
+  console.log("Solicitud recibida:", req.body);
+  res.status(200).json({ message: 'Recibido en el backend 🚀' });
 });
